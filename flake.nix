0619470bd31dad9b36f533165ed3c4513d52e2d5
JM Bell <johnmichaelbell@gmail.com>
2026-07-21 11:05:26 -0400

    adding gdtoolkit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "GMTK2026 dev shell (uv only; gdtoolkit is installed via uv)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.uv ];
+        };
+      });
+}
